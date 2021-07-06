5586b73f2d508305b2f8dac3ebe642eae6503f56
ASUS <ASUS@DESKTOP-JALG73E>
2021-07-06 16:26:16 +0700

    Add topic 02

--- selenium/webdriver/Topic_01_Check_Environment.java
+++ selenium/webdriver/Topic_01_Check_Environment.java
@@ -5,17 +5,17 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
 
 public class Topic_01_Check_Environment {
-  
-  @BeforeClass
-  public void beforeClass() {
-  }
-  
-  @Test
-  public void f() {
-  }
 
-  @AfterClass
-  public void afterClass() {
-  }
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+	@Test
+	public void f() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
 
 }
---
+++ selenium/webdriver/Topic_02_Run_On_Browser.java
@@ -0,0 +1,34 @@
+package webdriver;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+
+public class Topic_02_Run_On_Browser {
+
+	WebDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.chrome.driver", "..\\WEDDRIVER_TMHOANG\\lib\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void TC_01() {
+		driver.get("http://live.demoguru99.com/");
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
