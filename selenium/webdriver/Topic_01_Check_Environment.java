56422e5b9d45e0633a54300a6c750c994502243b
ASUS <ASUS@DESKTOP-JALG73E>
2021-07-06 15:57:35 +0700

    Setup Environment

---
+++ selenium/webdriver/Topic_01_Check_Environment.java
@@ -0,0 +1,21 @@
+package webdriver;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Topic_01_Check_Environment {
+  
+  @BeforeClass
+  public void beforeClass() {
+  }
+  
+  @Test
+  public void f() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
